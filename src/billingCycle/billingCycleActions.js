5a49f9637786ea1a2b2718b235f54462f0ebb845
LeandroVeiga9 <leandro.veiga.clear@gmail.com>
2021-12-23 13:13:39 -0300

    add create action

--- src/billingCycle/billingCycleActions.js
+++ src/billingCycle/billingCycleActions.js
@@ -9,3 +9,10 @@ export function getList(){
         payload: request
     }
 }
+
+export function create(values){
+    Axios.post(`${BASE_URL}/billingCycles`, values)
+    return{
+        type: 'TEMP'
+    }
+}

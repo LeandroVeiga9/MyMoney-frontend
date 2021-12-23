51f6b67beac840d349534d9463100526c49c2c15
LeandroVeiga9 <leandro.veiga.clear@gmail.com>
2021-12-23 10:01:11 -0300

    dashboard without redux

--- src/main/Routes.jsx
+++ src/main/Routes.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Router, Route, Redirect, hashHistory } from "react-router";
 
 import Dashboard from "../dashboard/Dashboard";
+import Dashboard2 from "../dashboard2/Dashboard2";
 import BillingCycle from "../billingCycle/BillingCycle";
 
 export default props => (
     <Router history={hashHistory}>
         <Route path='/' component={Dashboard} />
+        <Route path='/dash2' component={Dashboard2} />
         <Route path='/billingCycles' component={BillingCycle} />
         <Route path='*' to='/' />
     </Router>

77c5cce635cf5464071b1154cc282da777af875d
LeandroVeiga9 <leandro.veiga.clear@gmail.com>
2021-12-23 12:33:33 -0300

    create BillingCycleList component

--- src/billingCycle/BillingCycle.jsx
+++ src/billingCycle/BillingCycle.jsx
@@ -10,6 +10,7 @@ import TabsHeader from '../common/tab/TabsHeader'
 import TabHeader from '../common/tab/TabHeader'
 import TabContent from "../common/tab/TabContent"
 import { selectTab, showTabs } from "../common/tab/tabActions"
+import BillingCycleList from "./BillingCycleList";
 
 class BillingCycle extends Component{
 
@@ -31,7 +32,9 @@ class BillingCycle extends Component{
                             <TabHeader label='Excluir' icon="trash-o" target='tabDelete' />
                         </TabsHeader>
                         <TabsContent>
-                            <TabContent id='tabList'><h1>Lista</h1></TabContent>
+                            <TabContent id='tabList'>
+                                <BillingCycleList />
+                            </TabContent>
                             <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
                             <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
                             <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>

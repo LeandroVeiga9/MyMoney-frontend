950911a44b5b4b91a98424aedf2fe565bb2dfd89
LeandroVeiga9 <leandro.veiga.clear@gmail.com>
2021-12-27 17:11:38 -0300

    refactoring routes

--- src/main/Routes.jsx
+++ src/main/Routes.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-import { Router, Route, Redirect, hashHistory } from "react-router";
+import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
+import App from './App'
 import Dashboard from "../dashboard/Dashboard";
 import Dashboard2 from "../dashboard2/Dashboard2";
 import BillingCycle from "../billingCycle/BillingCycle";
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={Dashboard} />
-        <Route path='/dash2' component={Dashboard2} />
-        <Route path='/billingCycles' component={BillingCycle} />
+        <Route path='/' component={App}>
+            <IndexRoute component={Dashboard} />
+            <Route path='dash2' component={Dashboard2} />
+            <Route path='billingCycles' component={BillingCycle} />
+        </Route>
+        
         <Route path='*' to='/' />
     </Router>
 )

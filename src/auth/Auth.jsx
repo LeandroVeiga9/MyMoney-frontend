199a4ffa4cc91dd4ced165a1ac2ca3c9f0cac1c8
LeandroVeiga9 <leandro.veiga.clear@gmail.com>
2021-12-28 15:06:00 -0300

    AuthOrApp component

--- src/auth/Auth.jsx
+++ src/auth/Auth.jsx
@@ -6,11 +6,11 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { login, signup } from './authActions'
 
-import Row from '../common/layout/row'
-import Grid from '../common/layout/grid'
-import If from '../common/operator/if'
-import Messages from '../common/msg/messages'
-import Input from '../common/form/inputAuth'
+import Row from '../common/layout/Row'
+import Grid from '../common/layout/Grid'
+import If from '../common/operator/If'
+import Messages from '../common/msg/Messages'
+import Input from '../common/form/InputAuth'
 
 class Auth extends Component {
     constructor(props) {
--- src/main/Routes.jsx
+++ src/main/Routes.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
 
-import App from './App'
+import AuthOrApp from './AuthOrApp'
 import Dashboard from "../dashboard/Dashboard";
 import Dashboard2 from "../dashboard2/Dashboard2";
 import BillingCycle from "../billingCycle/BillingCycle";
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={App}>
+        <Route path='/' component={AuthOrApp}>
             <IndexRoute component={Dashboard} />
             <Route path='dash2' component={Dashboard2} />
             <Route path='billingCycles' component={BillingCycle} />
